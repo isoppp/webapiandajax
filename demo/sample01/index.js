9ddf52c24362cdf9889332a2406c9678f7a9e0b9
礒谷　浩生 <hiroki_isogai@waku-2.com>
2016-06-20 19:41:48 +0900

    mod demo01

--- demo/sample01/index.js
+++ demo/sample01/index.js
@@ -6,6 +6,7 @@ $(function () {
   var $requestBtn = $('.js-request');
   var $setResponse = $('.js-setResponse');
   var defaultText = $requestBtn.text();
+  var $template = $('<li>').addClass('list-group-item');
 
   function request() {
     $requestBtn.text('loading...' + ' ' + url);
@@ -23,12 +24,10 @@ $(function () {
 
     console.log(json);
     json.forEach(function (item) {
-      $setResponse.append($('<li>').text('Item!!!'));
-      $setResponse.append($('<li>').text(item.name));
-      $setResponse.append($('<li>').text(item.email));
+      $setResponse.append($template.clone().text('Item!!!'));
+      $setResponse.append($template.clone().text(item.name));
+      $setResponse.append($template.clone().text(item.email));
     });
-    $setResponse.find('li').addClass('list-group-item');
-
   }
 
   function error() {
